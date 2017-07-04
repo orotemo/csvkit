f49e96579c888ca49db2dda7557d502b8c5b5dd5
yinbal <yefet.inbal@gmail.com>
2017-07-04 13:03:26 +0300

    supporting carriage return (13) character appearing without line break (10)

--- src/csv.erl
+++ src/csv.erl
@@ -19,7 +19,10 @@ read([10|String], Acc) ->
   read(Rest, [Line|Acc]);
 read([13, 10|String], Acc) ->
   {Line, Rest} = read_line(String),
-  read(Rest, [Line|Acc]).
+  read(Rest, [Line|Acc]);
+read([13|String], Acc) ->
+ io:format(user,"~p, ~p~n",[String,Acc]),
+  read(String, Acc).
 
 add_spaces(0, String) -> String;
 add_spaces(Count, String) -> add_spaces(Count-1, [$ |String]).
@@ -48,8 +51,6 @@ read_line(String) -> read_line(String,[]).
 read_line([10|T], Acc) -> {lists:reverse(Acc), [10|T]};
 read_line([13,10|T], Acc) -> {lists:reverse(Acc), [13|T]};
 read_line([], Acc) -> {lists:reverse(Acc), []};
-read_line([13|T], Acc) ->
- io:format(user,"~p, ~p~n",[T,Acc]), {lists:reverse(Acc), [13|T]};
 read_line(String, []) -> 
   {Item, Rest} = read_item(String), read_line(Rest, [Item]);
 read_line([$,|String], Acc) -> 
