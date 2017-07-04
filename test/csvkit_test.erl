7547422b8c1271c04ffa23d28bb2a1c83569c539
yinbal <yefet.inbal@gmail.com>
2017-07-04 13:24:15 +0300

    supporting carriage return (13) character appearing without line break (10)

--- test/csvkit_test.erl
+++ test/csvkit_test.erl
@@ -4,11 +4,27 @@
 
 -define(COLUMNS, ["column2", "column3"]).
 %%% testing csvkit according to file test/csv_example.csv
+-define(CRCOLUMNS, ["sku", "short_description", "category_list", "was_price"
+                  "sell_price", "lalt1", "description", "currency", "gender"]).
 
 csvkit_file_test_() ->
   Filename = <<"test/csv_simple_example.csv">>,
   csvkit:parse_file(Filename, ?COLUMNS, fun send_result/3, 10),
   csvkit_all_tests().
+
+csvkit_file_carrige_return_test_() ->
+  Filename = <<"test/csv_cr_example.csv">>,
+  csvkit:parse_file(Filename, ?CRCOLUMNS, fun send_result/3, 10),
+  First = receive_once(),
+  Second = receive_once(),
+  Third = receive_once(),
+  Forth = receive_once(),
+
+  [?_assertMatch({1, {["AZZ00043"|_], _}, 10}, First),
+   ?_assertMatch({2, {["WZZ60324"|_], _}, 11}, Second),
+   ?_assertMatch({3, {["WZZ60324-124-34"|_], _}, 12}, Third),
+   ?_assertMatch({4, done, 13}, Forth)
+  ].
 
 csvkit_string_test_() ->
   Filename = <<"test/csv_simple_example.csv">>,
